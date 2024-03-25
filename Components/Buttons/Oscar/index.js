db5490eff80e4e77f3059c02cbd8b22c37905b99
1medi <144403286+1medi@users.noreply.github.com>
2024-03-24 23:35:51 -0700

    Added some pages / simple components
    i love dynamic content design

---
+++ Components/Buttons/Oscar/index.js
@@ -0,0 +1,9 @@
+import styles from "./Oscar.module.css"
+
+export default function Oscar(){
+  return (
+    <>
+    <button className={styles.button}>Tap in</button>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import Header from "@/Components/Header";
+import NavBar from "@/Components/Navbar";
 
 export default function Home() {
   return (
@@ -11,8 +13,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-
+      <Header/>
+      <main className={`${styles.main}`}>
+      <NavBar/>
       </main>
     </>
   );
