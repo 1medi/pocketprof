cc3a61563950126938ab41588734f6a24a308aa9
1medi <144403286+1medi@users.noreply.github.com>
2024-04-09 13:01:35 -0700

    adding nav bar icons / nd stuff

--- pages/QuizPage.js
+++ pages/QuizPage.js
@@ -14,20 +14,19 @@ export default function QuizPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
       <main className={`${styles.main}`}>
-
-      <div className={styles.container}>
-        <h1 className={styles.title}>Quiz App</h1>
-        <div className={styles.descriptionContainer}>
-          <h3>ARE YOU READY TO WIN</h3>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Quiz App</h1>
+          <div className={styles.descriptionContainer}>
+            <h3>ARE YOU READY TO WIN</h3>
+          </div>
+          <Link href='/quiz/page' className={styles.buttonContainer}>
+            <Button3 name={"Press To Start"} />
+          </Link>
         </div>
-        <Link href='/quiz/page' className={styles.buttonContainer}>
-        <Button3 name={"Press To Start"}/>
-        </Link>
-      </div>
       </main>
-      <NavBar/>
+      <NavBar />
     </>
   );
 }
