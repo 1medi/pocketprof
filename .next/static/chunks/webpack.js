48b129dda1019f5e4e6a0c1d20a99a4f9e69ccfb
Irinalim <ylim33@my.bcit.ca>
2024-04-13 23:10:20 -0700

    stylizing quiz page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f49210b52193ed84"; }
+/******/ 		__webpack_require__.h = function() { return "818051b2fca60865"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
