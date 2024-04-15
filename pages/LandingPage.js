a86c9d9ffd34f1f0071b47e5cc6e2e3cb94e5fd9
1medi <144403286+1medi@users.noreply.github.com>
2024-04-15 04:46:43 -0700

    stuff

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,41 +1,37 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "@/styles/LandingPage.module.css";
 import axios from 'axios';
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Header from "@/Components/Header";
+import NavBar from "@/Components/Navbar";
 
 export default function LandingPage() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [firstArticleTitle, setFirstArticleTitle] = useState('');
 
-  var type = 'How To Paint';
+  var type = 'Artificial Intelligence';
   var sortBy = 'publishedAt';
   var apiKey = process.env.NEXT_PUBLIC_API_KEY;
   var date = '2024-04-13';
   const url = `https://newsapi.org/v2/everything?q=${type}&from=${date}&sortBy=${sortBy}&apiKey=${apiKey}`;
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await axios.get(url);
-        const articles = response.data.articles;
-        setData(articles);
-        if (articles.length > 0) {
-          setFirstArticleTitle(articles[0].title);
-        }
-      } catch (err) {
-        console.error(err);
-        setError('Failed to fetch data');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchNews();
-  }, []);
+  const grabNews = async () => {
+    setIsLoading(true);
+    setError(null); // Clear previous errors
+    try {
+      const response = await axios.get(url);
+      console.clear();
+      setData(response.data.articles); 
+      console.log(response.data.articles);
+    } catch (err) {
+      console.error(err);
+      setError('Failed to fetch data');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -45,26 +41,24 @@ export default function LandingPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <div className={styles.mobileContainer}>
+      <Header name={"Articles"}/>
+      <main className={`${styles.main}`}>
+        <button onClick={grabNews} disabled={isLoading} className={styles.button}>
+          {isLoading ? 'Loading...' : 'Search'}
+        </button>
         {error && <p>{error}</p>}
-        {firstArticleTitle && (
-          <h1>{firstArticleTitle}</h1>
-        )}
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <div>
-            {data.map((article, index) => (
-              <div key={index} className={styles.article}>
-                <div className={styles.author}><p>Author: {article.author}</p></div>
-                <div className={styles.title}><p>Title: {article.title}</p></div>
-                <div className={styles.content}><p>Content: {article.content}</p></div>
-                <div className={styles.url}><p>URL: <a href={article.url}>Read more</a></p></div>
-              </div>
-            ))}
-          </div>
-        )}
+        {data.map((article, index) => (
+          <div key={index} className={styles.article}>
+            <div className={styles.author}><p>Author: {article.author}</p></div>
+            <div className={styles.title}><p>Title: {article.title}</p></div>
+            <div className={styles.content}><p>Content: {article.content}</p></div>
+            <div className={styles.url}></div><p>URL: <a href={article.url}>Read more</a></p></div>
+        ))}
       </main>
+      <NavBar/>
+      </div>
+
     </>
   );
 }
