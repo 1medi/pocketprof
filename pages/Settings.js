873b1464d6a37cc2890f556d72b71961a1d33310
Irinalim <ylim33@my.bcit.ca>
2024-04-21 21:50:37 -0700

    adding onboaring page and put components to each page

--- pages/Settings.js
+++ pages/Settings.js
@@ -24,6 +24,7 @@ export default function Home() {
       <main className={`${styles.main} ${montserrat.className}`}>
         <div className={styles.container}>
             <div className={styles.imageContainer}>
+            <Header name="Account"/>
               <Image
               src={Image1}
               width={275}
