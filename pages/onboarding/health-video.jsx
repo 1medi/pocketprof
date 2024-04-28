027b96ab381c3b9643db7211af04d0ab76319b2d
1medi <144403286+1medi@users.noreply.github.com>
2024-04-28 01:23:49 -0700

    adding to onboarding

--- pages/onboarding/health-video.jsx
+++ pages/onboarding/health-video.jsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 const YOUTUBE_PLAYLIST__ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems'
 
 export async function getServerSideProps(){
-  const res = await fetch(`${YOUTUBE_PLAYLIST__ITEMS_API}?part=snippet&playlistId=PLwWOafc04Z8y8TEYJIg9lWU5iuslL0_ZK&key=${process.env.YOUTUBE_API_KEY}`);
+  const res = await fetch(`${YOUTUBE_PLAYLIST__ITEMS_API}?part=snippet&playlistId=PLwWOafc04Z8xdxC3ZAqbN6CynnYUZF5An&key=${process.env.YOUTUBE_API_KEY}`);
   const data = await res.json();
   return {
     props: {
@@ -26,11 +26,11 @@ console.log('data', data)
           const { medium = {} } = thumbnails;
           return (
             <li key = {id} className={styles.card}>
-              <a href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}>
-              <p>
+              <a className={styles.imageContainer} href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}>
+              <p >
                 <img width={medium.width} height={medium.height} src={medium.url} alt=""></img>
               </p>
-              <h3>{ title }</h3>
+              <h3 className={styles.cardTitle}>{ title }</h3>
               </a>
             </li>
           )
