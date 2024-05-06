b4ff37d9238f0ae837e2459327811c5b85b2dcb9
1medi <144403286+1medi@users.noreply.github.com>
2024-05-06 07:10:22 -0700

    ng

--- pages/appSettings.js
+++ pages/appSettings.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "@/styles/AppSettings.module.css";
 import { Montserrat } from "next/font/google";
 import Header from "@/Components/Header";
