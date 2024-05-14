9fc50b8f660adc2b5e14b363325b71427169f0ea
1medi <144403286+1medi@users.noreply.github.com>
2024-05-13 23:38:27 -0700

    average

--- Components/CourseQuestions/index.js
+++ Components/CourseQuestions/index.js
@@ -20,7 +20,7 @@ export default function Chat() {
           <main>
             <div className={styles.textContainer}>
               <h2 className={styles.header}>What is your current skill level with Guitar?</h2>
-              <p className={styles.subHeader}>Help Oscar help you!</p>
+              <p className={styles.subHeader}>Let Oscar help you!</p>
             </div>
 
             <div className={styles.promptContainer}>
@@ -31,7 +31,7 @@ export default function Chat() {
               <Link
                 href={"/ShowResources"}
               >
-                <Button3 className={styles.prompt} name={"Intermediate"} />
+                <Button3 className={styles.prompt} name={"Average"} />
               </Link>
               <Link
                 href={"/ShowResources"}
