d6d509f092d68b9a2e9171262999501cf5d4740b
kennyharman <144394021+kennyharman@users.noreply.github.com>
2024-05-13 20:38:55 -0700

    more homepage fixes

--- Components/SubjectCard/index.js
+++ Components/SubjectCard/index.js
@@ -2,12 +2,12 @@ import styles from "./SubjectCard.module.css";
 
 export default function SubjectCard({ card }) {
     return (
-        <div className={styles.card__container} style={{backgroundImage: `url("${card.image}")`}}>
+        <button className={styles.card__container} style={{backgroundImage: `url("${card.image}")`}}>
             <div className={styles.card__title__container}>
                 <h5 className={styles.card__title}>
                     {card.title}
                 </h5>
             </div>
-        </div>
+        </button>
     );
 }
